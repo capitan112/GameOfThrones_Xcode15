92c154b79d8af176c8d95e8ecad3f8b46681fadf
Alexei Chebotarov <oleksiy.chebotarov.ext@bbc.co.uk>
2024-10-22 13:54:59 +0100

    Initial Commit

---
+++ GameOfThronesXcode15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameOfThronesXcode15
+//
+//  Created by Oleksiy Chebotarov on 22/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
